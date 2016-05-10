4a2b8c94afded6ce3def515199b129b0fa89aaa2
Fred Lundén <fredlunden@gmail.com>
2016-05-10 22:59:58 +0200

    debug routes

--- api/routes.js
+++ api/routes.js
@@ -24,7 +24,7 @@ module.exports = function(cartman) {
       var msg = text[1].trim();
 
 
-      if (cmd.match('(?:\d{4}-\d{2}-\d{2})')) {
+      if (date.match('(?:\d{4}-\d{2}-\d{2})')) {
         console.log('correct date');
         cartman.commands.saveStatus(username, userId, date, msg);
       } else {
