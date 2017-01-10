f133c11ea994ec64fe9c33461175f355f031d2cc
fredlunden <fred@softresource.se>
2017-01-10 12:11:50 +0100

    added builds api call

--- api/routes.js
+++ api/routes.js
@@ -8,10 +8,25 @@ module.exports = function(robopoc) {
 
   router.get('/test', function(req, res) {
     var json = req.body;
+
   });
 
   router.post('/', function(req, res, next) {
     console.log('api: /');
+    var builds = req.body;
+
+    var success = 0;
+    var fail = 0;
+    for (var i = 0; i < builds.length) {
+      var build = builds[i];
+      if (build.Status === 'FAILURE') {
+        fail++;
+      } else {
+        succes++;
+      }
+    }
+    robopoc.sendMessage(success + ' succesfull builds');
+    robopoc.sendMessage(fail + ' failed builds');
     res.send({ success: true });
   });
 
@@ -24,7 +39,7 @@ module.exports = function(robopoc) {
     console.log('api: /latestFailed');
     try {
       var build = req.body; //json
-      robopoc.sendMessage(build.ProjectName + ' ' + build.LastModifiedBy);
+      robopoc.sendMessage(build.LastModifiedBy + ' broke the build! [Project]: ' + build.ProjectName);
       res.send({ success: true });
     } catch (err) {
       res.send({ success: false });
